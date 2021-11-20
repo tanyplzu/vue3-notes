a9f5e5181668b5aa083e2c8555dd1881158f4fcf
tanyp <759227027@qq.com>
2021-11-20 22:39:36 +0800

    feat: 笔记更新

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'vue3-note',
-      script: 'server.js'
+      script: 'server.js',
     },
   ],
   deploy: {
@@ -11,11 +11,13 @@ module.exports = {
       host: '47.97.10.178',
       ref: 'origin/master',
       repo: 'git@github.com:tanyplzu/vue3-notes.git',
-      path: '/home/project/vue3-notes',
-      'post-deploy': 'git reset --hard && git checkout master && git pull && npm i --production=false && npm run docs:build && pm2 startOrReload ecosystem.config.js', 
+      path: '/www/website/vue3-notes',
+      ssh_options: 'StrictHostKeyChecking=no',
+      'post-deploy':
+        'git checkout master && git pull && npm i --production=false && npm run docs:build && pm2 startOrReload ecosystem.config.js',
       env: {
-        NODE_ENV: 'production'
-      }
-    }
-  }
-}
+        NODE_ENV: 'production',
+      },
+    },
+  },
+};
--- server.js
+++ server.js
@@ -1,6 +1,15 @@
-const server = require('pushstate-server');
-
-server.start({
-  port: 8082,
-  directory: './dist',
+const express = require('express');
+const compression = require('compression');
+// 端口可以自己定义
+const port = process.env.PORT || 3002;
+const app = express();
+// 开启 gzip 压缩
+app.use(compression());
+app.use(express.static('./dist'));
+module.exports = app.listen(port, function (err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log('Listening at ' + port + '\n');
 });
